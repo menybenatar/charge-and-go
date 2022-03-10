fed18cf081e8d1436e00c7125cfec08822c71b02
menybenatar <menybenatar@gmail.com>
2022-03-11 01:26:36 +0200

    first commit

---
+++ charge-and-go/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Login from "./login";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
